ccb03912a142874d3bbe1d5bfab88934696f84a5
safir <safir.medjeber@gmail.com>
2020-12-04 20:47:27 +0100

    last solution

--- src/main/java/TextWrapper.java
+++ src/main/java/TextWrapper.java
@@ -1,16 +1,13 @@
 class TextWrapper {
 
-
     public static String wrap(int width, String text) {
         if (text == null)
             return "";
         if (text.length() <= width) {
-            return text.trim();
+                       return text.trim();
         }
 
-        String l = "ee";
-        if(l.equals(""))
-            return"lol";
+
         return breakTextIntoLines(width, text);
 
     }
